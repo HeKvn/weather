96151764a22e70758d013ca6cb3760532426066f
HeKvn <377014982@qq.com>
2020-09-25 10:16:47 +0800

    第一次提交

---
+++ src/index.js
@@ -0,0 +1,26 @@
+var app = new Vue({
+    el:"#app",
+    data:{
+        city:'',
+        weather:[]
+    },
+    methods:{
+        searchWeather:function(){
+            const that = this
+            // console.log('天气查询')
+            // console.log(this.city)
+            axios.get('http://wthrcdn.etouch.cn/weather_mini?city='+this.city)
+            .then(function(respone){
+                // console.log(respone.data.data.forecast)
+                that.weather = respone.data.data.forecast
+            })
+            .catch(function(err){
+                console.log(err)
+            })
+        },
+        changeCity:function(city){
+            this.city = city
+            this.searchWeather()
+        }
+    }
+})
